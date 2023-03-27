39e0b8d2b874114b810e8a7b0f5f4e7b7b08ad36
unknown <Gary@EGL-2021-A3.energygo.local>
2023-03-27 11:19:59 +0200

    telechargement

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import '../css/login.css'
 import { useNavigate,Link } from 'react-router-dom'
 import initConnexion from '../firebase';
@@ -7,12 +7,17 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 initConnexion();
 
 function LoginPage() {
-  
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
   const navigate = useNavigate();
   const auth = getAuth();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+    });
+  }, []);
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
@@ -20,7 +25,19 @@ function LoginPage() {
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
-
+  const handleClick = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User installed the app');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+        setDeferredPrompt(null);
+      });
+    }
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
     signInWithEmailAndPassword(auth, email, password)
@@ -44,6 +61,7 @@ function LoginPage() {
 
   return (
     <div className="login-page">
+      <div className="cadre">
       <form onSubmit={handleSubmit}>
         <h2>Login</h2>
         <label>
@@ -59,7 +77,13 @@ function LoginPage() {
         
         <br />
         <Link className="button" to="/signup">Créer un compte (En maintenance)</Link>
+        <br />
       </form>
+      <br />
+      <button onClick={handleClick} className="button">
+      Télécharger l'application
+    </button>
+    </div>
     </div>
   );
 }
